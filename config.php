add804b11a96e186f903be928dc13b4191d59b7d
Omar Yehia Ahmed <62802456+OmarYehia@users.noreply.github.com>
2021-03-09 23:59:43 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-define("__WEATHER_URL","http://api.openweathermap.org/data/2.5/weather?id={{cityid}}&lang=en&units=metric&APPID=7d938c2e126554bcd611e9d95495e0ad");
+define("__WEATHER_URL","http://api.openweathermap.org/data/2.5/weather?id={{cityid}}&lang=en&units=metric&APPID={{apiid}}");
 define("__CITIES_FILE", "resources/city.list.json");
 
 // SMS
-define("__TWILIO_ACCOUNT_SID", "ACbbd596ecc1b983c35904476a76e90201");
-define("__TWILIO_AUTH_TOKEN", "577e5d9b685c7c75dcf19eaa4b65baec");
-define("__TWILIO_MY_PHONE_NUMBER", "+15632783503");
+define("__TWILIO_ACCOUNT_SID", "{{accountsid}}");
+define("__TWILIO_AUTH_TOKEN", "{{authtoken}}");
+define("__TWILIO_MY_PHONE_NUMBER", "{{phonenumber}}");
